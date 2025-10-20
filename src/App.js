41ee445d05f6660962be0e32501ecec28d191fc2
muneeb-git-023 <muneeburrahman024>
2025-10-20 17:32:05 +0500

    changes the code

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ export default function CounterApp() {
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>React Counter App</h1>
       <h2>{count}</h2>
-      <button onClick={() => setCount(count + 1)}>+</button>
-      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={() => setCount(count + 1)}>-</button>
+      <button onClick={() => setCount(count - 1)}>+</button>
       <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
